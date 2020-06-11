fc22152874474241e27e227feb7e146918dabc39
Eduardo Loo <eduardo.loo@pucp.pe>
2020-06-11 11:31:35 -0500

    Fin seccion 5

--- sockets/socket.ts
+++ sockets/socket.ts
@@ -1,10 +1,20 @@
 import { Socket } from 'socket.io';
 import socketIO from 'socket.io';
+import { UsuariosLista } from '../classes/usuarios-lista';
+import { Usuario } from '../classes/usuario';
+
+export const usuariosConectados = new UsuariosLista();
+
+export const conectarCliente = ( cliente: Socket ) => {
+    const usuario = new Usuario( cliente.id );
+    usuariosConectados.agregar( usuario );
+}
 
 export const desconectar = ( cliente: Socket ) => {
     
     cliente.on('disconnect', () => {
         console.log('Cliente desconectado');
+        usuariosConectados.borrarUsuario( cliente.id );
     });
 }
 
@@ -15,5 +25,20 @@ export const mensaje = ( cliente: Socket, io: socketIO.Server ) => {
         console.log('Mensaje recibido', payload);
 
         io.emit('mensaje-nuevo', payload );
+    });
+}
+
+// Configurar usuario
+export const configurarUsuario = ( cliente: Socket, io: socketIO.Server ) => {
+
+    cliente.on('configurar-usuario', ( payload: { nombre: string }, callback: Function ) => {
+        // console.log('Configurando Usuario', payload);
+        usuariosConectados.actualizarNombre( cliente.id, payload.nombre );
+
+        // io.emit('mensaje-nuevo', payload );
+        callback({
+            ok: true,
+            mensaje: `Usuario ${ payload.nombre }, configurado`
+        })
     });
 }
