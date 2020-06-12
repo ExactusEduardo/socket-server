bedeae7a568c354a735e79e6239b758e9475e7ac
Eduardo Loo <eduardo.loo@pucp.pe>
2020-06-11 23:21:52 -0500

    Seccion 6 Lista

--- sockets/socket.ts
+++ sockets/socket.ts
@@ -5,40 +5,46 @@ import { Usuario } from '../classes/usuario';
 
 export const usuariosConectados = new UsuariosLista();
 
-export const conectarCliente = ( cliente: Socket ) => {
+export const conectarCliente = ( cliente: Socket, io: socketIO.Server ) => {
     const usuario = new Usuario( cliente.id );
     usuariosConectados.agregar( usuario );
+    io.emit('usuarios-activos', usuariosConectados.getLista() );
 }
-
-export const desconectar = ( cliente: Socket ) => {
+export const desconectar = ( cliente: Socket, io: socketIO.Server ) => {
     
     cliente.on('disconnect', () => {
         console.log('Cliente desconectado');
         usuariosConectados.borrarUsuario( cliente.id );
     });
 }
-
 // Escuchar mensajes
 export const mensaje = ( cliente: Socket, io: socketIO.Server ) => {
 
     cliente.on('mensaje', ( payload: { de: string, cuerpo: string } ) => {
         console.log('Mensaje recibido', payload);
-
         io.emit('mensaje-nuevo', payload );
     });
 }
-
 // Configurar usuario
 export const configurarUsuario = ( cliente: Socket, io: socketIO.Server ) => {
 
     cliente.on('configurar-usuario', ( payload: { nombre: string }, callback: Function ) => {
         // console.log('Configurando Usuario', payload);
         usuariosConectados.actualizarNombre( cliente.id, payload.nombre );
-
+        io.emit('usuarios-activos', usuariosConectados.getLista() );
         // io.emit('mensaje-nuevo', payload );
         callback({
             ok: true,
             mensaje: `Usuario ${ payload.nombre }, configurado`
         })
     });
+}
+// Obtener Usuarios
+export const obtenerUsuarios = ( cliente: Socket, io: socketIO.Server ) => {
+
+    cliente.on('obtener-usuarios', ()  => {
+        console.log('Obtener Usuarios');
+        io.to( cliente.id ).emit('usuarios-activos', usuariosConectados.getLista() );
+    });
+
 }
